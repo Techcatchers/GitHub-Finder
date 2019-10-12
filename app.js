96857de4b96210922facd39dee91b6154418a394
Rishabh Agrawal <rishabha.1999@gmail.com>
2019-10-12 15:02:37 +0530

    Restrict spaces input field

--- app.js
+++ app.js
@@ -2,39 +2,42 @@ const github = new GitHub();
 const ui = new UI();
 let execute;
 
-const searchUser = document.querySelector('#search-user');
+const searchUser = document.querySelector("#search-user");
 
-searchUser.addEventListener('keyup', (e) => {
-    clearTimeout(execute);
-    const userText = e.target.value;
-    execute = setTimeout(extractInfo, 1000, userText);
-})
+searchUser.addEventListener("keydown", e => {
+    // Disable spaces in input field
+    if (e.which === 32)
+        e.preventDefault();
+});
 
+searchUser.addEventListener("keyup", e => {
+  clearTimeout(execute);
+  const userText = e.target.value;
+  execute = setTimeout(extractInfo, 1000, userText.trim());
+});
 
 function extractInfo(userText) {
-
-    if (userText !== '') {
-        ui.showLoading();
-        github.getuser(userText)
-        .then (data => {
-            ui.hideLoading();
-            if ((data.profile.message) === 'Not Found') {
-                // show alert
-                // console.log(data);
-                ui.showAlert('User not found', 'alert alert-danger');
-            } else {
-                //show profile
-                ui.showProfile(data.profile);
-                ui.showRepos(data.repos);
-            }
-        })
-        .catch (() => {
-            ui.hideLoading();
-            ui.showAlert('User not found', 'alert alert-danger');
-        })
-    } else {
-        // clear profilw
-        ui.clearProfile();
-
-    }
+  if (userText !== "") {
+    ui.showLoading();
+    github
+      .getuser(userText)
+      .then(data => {
+        ui.hideLoading();
+        if (data.profile.message === "Not Found") {
+          // show alert
+          ui.showAlert("User not found", "alert alert-danger");
+        } else {
+          // show profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+      .catch(() => {
+        ui.hideLoading();
+        ui.showAlert("User not found", "alert alert-danger");
+      });
+  } else {
+    // clear profilw
+    ui.clearProfile();
+  }
 }
